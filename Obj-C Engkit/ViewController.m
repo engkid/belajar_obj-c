ea7d861351c34de788d9117134def3dddba12cdc
Engkit Satia Riswara <engkit_sr643@mitrais.com>
2018-09-24 14:19:49 +0700

    Initial Commit

---
+++ Obj-C Engkit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Obj-C Engkit
+//
+//  Created by Engkit Satia Riswara on 24/09/18.
+//  Copyright © 2018 Engkit Satia Riswara. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Obj-C Engkit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Obj-C Engkit
+//
+//  Created by Engkit Satia Riswara on 24/09/18.
+//  Copyright © 2018 Engkit Satia Riswara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
